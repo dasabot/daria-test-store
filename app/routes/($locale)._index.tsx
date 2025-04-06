0bf5db29d1f7a9b80b1557f841d8835a550b3d10
dasabot <dasabot@icloud.com>
2025-04-06 23:24:11 +0300

    change styles link in the Homepage

--- app/routes/($locale)._index.tsx
+++ app/routes/($locale)._index.tsx
@@ -1,6 +1,13 @@
+import { Link } from '@remix-run/react'
+
 const Homepage = () => (
   <div className="home">
-    <a href="/collections/sale">Go to the Sale collection</a>
+    <Link
+      className="inline-block bg-white border border-gray-200 shadow-inner text-gray-800 px-[20px] py-[10px] rounded-full hover:bg-gray-100 transition-all duration-300 font-medium"
+      to="/collections/sale"
+    >
+      Go to the Sale collection
+    </Link>
   </div>
 )
 
